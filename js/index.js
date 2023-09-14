b0e5aa132d5b8a85e7a9807f613dd79bf5d5c96c
MykRub <misha.rubaha@gmail.com>
2023-09-14 19:50:22 +0200

    new

---
+++ js/index.js
@@ -0,0 +1,45 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    speed: 800,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+VANTA.NET({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 250.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x0,
+    backgroundColor: 0xb6b6b6,
+    showDots: false,
+})
+VANTA.WAVES({
+    el: "#radio",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 135.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xd111b,
+    waveHeight: 4.00,
+    waveSpeed: 2.00,
+    zoom: 1.75
+})
